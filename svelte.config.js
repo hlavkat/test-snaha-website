4f2734c9afe1f0f9d79ca700d14f4b9be5c43328
hlavkat <hlavkat@gmail.com>
2025-02-25 10:55:23 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static'
 
 export default {
 	kit: {
-		paths: { relative: false },
+		paths: { base: '/test-snaha-website' },
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
