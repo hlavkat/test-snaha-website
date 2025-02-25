559407f27c12b79fd364951ad16254494f4a08ca
hlavkat <hlavkat@gmail.com>
2025-02-25 10:30:09 +0100

    fix: path to img

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static'
 
 export default {
 	kit: {
+		paths: { relative: false },
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
